f47c190cf4dc6fc7cacf78683af8f64fdcdbcea6
DeazOut <chupichuros10akk@yandex.ru>
2025-07-21 22:51:41 +0300

    product cards realisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Яблоко',
+          img: './pngimg.com - apple_PNG12507.png',
+          desc: 'Яблоко зеленое',
+          categore: 'fruits',
+          price: '60'
+        },
+        {
+          id: 2,
+          title: 'Банан',
+          img: './pngimg.com - banana_PNG104249.png',
+          desc: 'Банан из Судана',
+          categore: 'fruits',
+          price: '55'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+    <div className="wrapper">
+      <Header/>
+      <Items items={this.state.items}/>
+      <Footer/>
     </div>
   );
+  }
+  
 }
 
 export default App;
